1299b3d8416f3948676ed81e39228845bf039349
gustavo <gustavo_cunhaavila@hotmail.com>
2021-03-08 12:42:52 -0300

    Add vue client and vuetify

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-
 const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 const fs = require('fs');
 const path = require('path');
 const port = 4000;
 
+// Import typeDefs and resolvers
 const filepath = path.join(__dirname, 'typeDefs.gql');
 const typeDefs = fs.readFileSync(filepath, 'utf-8');
 const resolvers = require('./resolvers.js');
 
+// import Environment Variables and Mongoose Models
 require('dotenv').config({ path: 'variables.env' });
 const User = require('./models/user.js');
 const Post = require('./models/post.js');
 
+// Connect to MongoDB
 mongoose
     .connect(
         process.env.MONGO_URI,
@@ -21,8 +23,7 @@ mongoose
     .then(() => console.log('DB Conntected!'))
     .catch(err => console.error(err));
 
-
-
+// Create Apollo/GraphQL Server using typeDefs, resovlers and context object  
 const server = new ApolloServer({
     typeDefs,
     resolvers,
@@ -32,6 +33,6 @@ const server = new ApolloServer({
     }
 });
 
-server.listen(port).then(() => {
-    console.log("Server listening on %d...", port);
+server.listen(port).then(({ url }) => {
+    console.log(`Server listening on ${url}`);
 });
