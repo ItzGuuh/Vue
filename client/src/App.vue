3db89b9aa03fb0973d3083e180905de1e81541a2
gustavo <gustavo_cunhaavila@hotmail.com>
2021-03-08 23:16:05 -0300

    Add Vuetify with a working theme; Add Horizontal Navbar and Mobile First Design

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,73 @@
 <template>
-  <div>
-    <h1>App</h1>
-    <router-view/>
-  </div>
+  <v-app>
+    <!-- Horizontal navbar -->
+    <v-toolbar
+      fixed
+      color="primary"
+      dark
+    >
+      <!-- App Title -->
+      <vtoolbar-side-icon></vtoolbar-side-icon>
+      <v-toolbar-title class="hidden-xs-only">
+        <router-link
+          to="/"
+          tag="span"
+          style="cursor: pointer"
+        >
+          VueShare
+        </router-link>
+      </v-toolbar-title>
+
+      <v-spacer />
+
+      <!-- Search Input -->
+      <v-text-field
+        flex
+        prepend-icon="search"
+        placeholder="Search posts"
+        color="accent"
+        single-line
+        hide-details
+      />
+      <v-spacer />
+
+      <!-- Horizontal NavBar links -->
+      <vtoolbar-items class="hidden-xs-only">
+        <v-btn
+          flat
+          v-for="item in horizontalNavItems"
+          :key="item.title"
+          :to="item.link"
+        >
+          <v-icon
+            class="hidden-sm-only"
+            left
+          >{{item.icon}}</v-icon>
+          {{item.title}}
+        </v-btn>
+      </vtoolbar-items>
+    </v-toolbar>
+
+    <!-- App content -->
+    <main>
+      <v-container class="mt-4">
+        <router-view />
+      </v-container>
+    </main>
+  </v-app>
 </template>
+
+<script>
+export default {
+  name: "App",
+  computed: {
+    horizontalNavItems() {
+      return [
+        { icon: "chat", title: "Posts", link: "/posts" },
+        { icon: "lock_open", title: "Sing In", link: "/singin" },
+        { icon: "create", title: "Sign Up", link: "/signup" },
+      ];
+    },
+  },
+};
+</script>
---
+++ client/src/plugins/vuetify.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+
+Vue.use(Vuetify, {
+    theme: {
+        primary: "#2E1F27",
+        secondary: "#854D27",
+        accent: "#E7D8C9",
+        error: "#f44336",
+        warning: "#e91e63",
+        info: "#2196f3",
+        success: "#4caf50"
+    }
+});
