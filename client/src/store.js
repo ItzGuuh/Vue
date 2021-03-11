3655d00bdcfbb66f27dc130a5f42d73b69bcfb86
gustavo <gustavo_cunhaavila@hotmail.com>
2021-03-10 22:41:15 -0300

    Create queries.js for clientside query mutation definitions

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import {gql} from "apollo-boost";
 
+import { GET_POSTS} from "./queries";
 import {defaultClient as apolloClient} from './main'
 
 Vue.use(Vuex)
@@ -23,14 +23,7 @@ export default new Vuex.Store({
     getPosts: ({ commit }) => {
       commit('setLoading', true);
       apolloClient.query({
-        query: gql`query{
-          getPosts {
-            _id
-            title
-            imageUrl
-          }
-        }
-        `
+        query: GET_POSTS
       }).then(({data}) => {
         // Get data from actions via mutations
         // Commit passes date from actions along to mutation functions
